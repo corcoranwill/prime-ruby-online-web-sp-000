697cb3a75b2f63a2e0dc68b737e9dc5267c40b83
Will Corcoran <corcoranwill@gmail.com>
2020-02-18 01:04:29 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,9 @@
 # Add  code here!
 def prime?(num)
+  return if num <= 1
+  (2..Math.sqrt(num)).none? { |i| (num % i).zero? }
 
-  # return booleean - is number prime?
-
-  return false if num <= 1
-  Math.sqrt(num).to_i.downto(2).each {|i| return false if num % i == 0}
-  true
-
-
+  # return false if num <= 1
+  # Math.sqrt(num).to_i.downto(2).each {|i| return false if num % i == 0}
+  # true
 end
