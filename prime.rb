2f2fe6c509a5707da37e4099b2fd4b5c41426da2
Will Corcoran <corcoranwill@gmail.com>
2020-02-18 01:00:25 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+def prime(num)
+
+  # return booleean - is number prime?
+
+  return false if num <= 1
+  Math.sqrt(num).to_i.downto(2).each {|i| return false if num % i == 0}
+  true
+
+
+end 
