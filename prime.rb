c0cf60bbbc0788c49ba5a72fc8b2114dd291f7c3
Will Corcoran <corcoranwill@gmail.com>
2020-02-18 01:08:17 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,8 +2,4 @@
 def prime?(num)
   return false if num <= 1
   (2..Math.sqrt(num)).none? { |i| (num % i).zero? }
-
-  # return false if num <= 1
-  # Math.sqrt(num).to_i.downto(2).each {|i| return false if num % i == 0}
-  # true
 end
